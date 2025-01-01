a7ab467424277da50b045eba88dd47a898e781ed
joy30000 <Doraemon771714@gmail.com>
2025-01-01 23:56:20 +0530

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import About from './pages/about'
-import Contact from './pages/contact'
+// import About from './pages/about'
+// import Contact from './pages/contact'
 import Homepage from './pages/Homepage'
 import AnimeDetail from './pages/AnimeDetail'
 import Episode from './pages/Episode'
@@ -15,8 +15,8 @@ function App() {
     <>
     <Navbar/>
     <Routes>
-      <Route path='/about' element={<About/>} />
-      <Route path='/contact' element={<Contact/>} />
+      {/* <Route path='/about' element={<About/>} />
+      <Route path='/contact' element={<Contact/>} /> */}
       <Route path='/homepage' element={<Homepage/>} />
       <Route path='/anime_detail' element={<AnimeDetail/>} />
       <Route path='/anime_episode' element={<Episode/>}  />
--- src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function About() {
-  return (
-    <div className="underline text-3xl">About</div>
-  )
-}
-
-export default About
